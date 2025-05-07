8e64ea273105c73a96ccd13dbba27c83142bbd33
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-07 19:52:32 +0200

    added Functionality to Main Menu, Changes
    Main Menu
    - Play - starts a game scene
    - Options - starts an options scene with back button
    - Quit - exits game

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+const GAME = preload("res://Scenes/Game.tscn")
+const OPTIONS = preload("res://Scenes/Options.tscn")
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("Escape"):
-		
-		print()
+func _on_play_pressed() -> void:
+	get_parent().add_child(GAME.instantiate())
+	self.queue_free()
+
+
+func _on_options_pressed() -> void:
+	get_parent().add_child(OPTIONS.instantiate())
+	self.queue_free()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
