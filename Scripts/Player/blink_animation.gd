668a80e3e4dc112427216d6a48ae9428f42466b2
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-14 14:19:29 +0200

    blink ještě víc rozbitý 👍

---
+++ Scripts/Player/blink_animation.gd
@@ -0,0 +1,18 @@
+extends AnimationPlayer
+
+
+@export var amount_of_flash : float:
+	set(value):
+		amount_of_flash = value
+		$"..".material.set_shader_parameter("flash_color", amount_of_flash)
+
+
+
+func _physics_process(delta: float) -> void:
+	#$"..".material.set_shader_parameter("flash_color", amount_of_flash)
+	printerr($"..".material.get_shader_parameter("flash_color"))
+	
+
+
+func _ready() -> void:
+	$"..".material.set_shader_parameter("flash_color", 1)
