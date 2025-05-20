d8a6aa110a408b968f1067ccd80b205f1f582f1d
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-20 19:52:34 +0200

    add Cursor Size Option
    added Small, Medium, Large Cursor Size
    16x16. 32x32, 64x64 - respectivelly

--- Scripts/Menu Control/options.gd
+++ Scripts/Menu Control/options.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var main_menu: Node2D = $"../MainMenu"
+var smallCursor = load("res://Sprites/smallCursor.png")
+var medCursor = load("res://Sprites/medCursor.png")
+var defCursor = load("res://Sprites/defCursor.png")
 
 func _on_back_pressed() -> void:
 	self.hide()
@@ -22,3 +25,12 @@ func _on_toggle_full_screen_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 		return
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	if value == 1:
+		Input.set_custom_mouse_cursor(smallCursor, Input.CURSOR_ARROW, Vector2(8, 8))
+	if value == 2:
+		Input.set_custom_mouse_cursor(medCursor, Input.CURSOR_ARROW, Vector2(16, 16))
+	if value == 3:
+		Input.set_custom_mouse_cursor(defCursor, Input.CURSOR_ARROW, Vector2(32, 32))
