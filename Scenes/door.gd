ef1a50b5754850c615a34338f4c9b9aaa3da923a
freezerbox <100595871+freezerbox1@users.noreply.github.com>
2025-05-29 15:14:41 +0200

    door almost done

--- Scenes/door.gd
+++ Scenes/door.gd
@@ -1,4 +1,17 @@
 extends Node2D
+@onready var animation_player: AnimationPlayer = $TheDoor/AnimationPlayer
+@onready var open_timer: Timer = $OpenTimer
+@onready var close_timer: Timer = $CloseTimer
+#self.queue_free()
 
 func _on_open_area_body_entered(body: Node2D) -> void:
-	self.queue_free()
+	animation_player.play("flash")
+	open_timer.start()
+	
+	
+func _on_open_timer_timeout() -> void:
+	animation_player.play("open")
+	close_timer.start()
+	
+func _on_close_timer_timeout() -> void:
+	animation_player.play("opened")
