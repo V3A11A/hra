476c7f6606763e5cf0333a679ef1508990e19d2f
Cronecs <cronecs0@gmail.com>
2025-05-29 07:19:34 +0200

    gg maybe blink soon

--- Scripts/Player/blink_animation.gd
+++ Scripts/Player/blink_animation.gd
@@ -1,21 +1,21 @@
 extends AnimationPlayer
 
 
-#@export var amount_of_flash : float:
-	#set(value):
-		#amount_of_flash = value
-		#$"..".material.set_shader_parameter("flash_color", amount_of_flash)
+@export var amount_of_flash : float:
+	set(value):
+		amount_of_flash = value
+		$"..".material.set_shader_parameter("flash_value", amount_of_flash)
 
 
 
-#func _physics_process(delta: float) -> void:
-	#$"..".material.set_shader_parameter("flash_color", amount_of_flash)
-	#printerr($"..".material.get_shader_parameter("flash_color"))
+func _physics_process(delta: float) -> void:
+	$"..".material.set_shader_parameter("flash_value", amount_of_flash)
+	#printerr($"..".material.get_shader_parameter("flash_value"))
 
 
 
-func _ready() -> void:
-	$"..".material.set_shader_parameter("flash_color", 1)
+#func _ready() -> void:
+	#$"..".material.set_shader_parameter("flash_value", 1)
 
 
 func _on_health_system_damage_taken(amount: int) -> void:
