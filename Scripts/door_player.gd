da086fa6bbe31d6ba6b1823c4142be9870e73caf
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-30 18:55:26 +0200

    minor bugfix featuring audio and doors
    added functionality for player and enemy doors, fix looping moosic

--- Scripts/door_player.gd
+++ Scripts/door_player.gd
@@ -16,3 +16,14 @@ func _on_open_timer_timeout() -> void:
 	animation_player.play("open")
 	close_timer.start()
 	open_timer.stop()
+	
+	
+func _on_close_timer_timeout() -> void:
+	animation_player.play("close")
+	close_timer.stop()
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "close":
+		animation_started = !animation_started
