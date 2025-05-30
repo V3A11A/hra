81cca5990011557cca8cdafb6ed3319ee61f20ec
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-30 19:12:56 +0200

    fast fix

---
+++ Scenes/Player/node.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@onready var movement: CollisionShape2D = $".."
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../../AnimatedSprite2D"
+
+
+
+func _physics_process(delta: float) -> void:
+	if movement.run_velocity == Vector2.ZERO:
+		animated_sprite_2d.set_frame_and_progress(0, 0.0)
+	else:
+		animated_sprite_2d.animation
