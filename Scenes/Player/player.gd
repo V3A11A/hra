ab752e8d2fad2deb7c90c9ff874c7de58b52c4b5
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-05-31 18:34:41 +0200

    accidental fuckup

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -11,9 +11,9 @@ func _process(delta: float) -> void:
 
 func _on_health_system_obliterate() -> void:
 	#pass #Game over, pánové! 🫡
-	$"../../CanvasLayer/Game Over Screen/CanvasLayer/Speed".stop()
+	#$"../../CanvasLayer/Game Over Screen/CanvasLayer/Speed".stop()
 	$"../../CanvasLayer/Game Over Screen/CanvasLayer/KillCount".text = "[wave]kill count: " + str($"UI/GameStats".enemies_killed)
-	$"../../CanvasLayer/Game Over Screen/CanvasLayer/PlayTime".text = "[wave]time alive: " + str($"SpeedrunClock".get)
+	#$"../../CanvasLayer/Game Over Screen/CanvasLayer/PlayTime".text = "[wave]time alive: " + str($"SpeedrunClock".get)
 	game.process_mode = Node.PROCESS_MODE_DISABLED
 	game.get_node("../CanvasLayer/Game Over Screen").show()
 	game.get_node("../CanvasLayer/Game Over Screen/CanvasLayer").show()
