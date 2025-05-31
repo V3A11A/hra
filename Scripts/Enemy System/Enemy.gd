13d469448fb835da83a46d1e7630970607a638ab
LukSonyX <131042194+LukSonyX@users.noreply.github.com>
2025-06-01 00:56:11 +0200

    here goes blood

--- Scripts/Enemy System/Enemy.gd
+++ Scripts/Enemy System/Enemy.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @export var damage : int = 1
 @export var difficulty : int = 1
 
+var BloodStainTypes = [
+	preload("res://Scenes/bloodstain_dump.tscn"),
+	preload("res://Scenes/bloodstain_poly.tscn"),
+	preload("res://Scenes/bloodstain_spray.tscn")
+]
 
 @onready var wave_system: Node = get_tree().get_root().get_node("Main/Game/Wave System")
 @onready var health_system: Node = $"Health System"
@@ -15,6 +20,12 @@ func _exit_tree() -> void:
 
 func _on_health_system_obliterate() -> void:
 	$"../../Player/UI/GameStats".enemies_killed += 1
+	var randomnumber = randi() % BloodStainTypes.size()
+	var blood = BloodStainTypes[randomnumber].instantiate()
+	blood.position = position
+	blood.rotation_degrees = randf() * 360
+	blood.scale = Vector2.ONE * (0.8 + randf() * 0.4)
+	get_tree().current_scene.get_node("Game/Map").add_child(blood)
 	queue_free()
 
 func _process(delta: float) -> void:
